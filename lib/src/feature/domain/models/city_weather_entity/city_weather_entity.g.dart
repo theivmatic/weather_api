f4f765a910d77b0d998562e5fe106c59c9383f14
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2023-11-21 21:31:22 +0500

    generated new model

--- lib/src/feature/domain/models/city_weather_entity/city_weather_entity.g.dart
+++ lib/src/feature/domain/models/city_weather_entity/city_weather_entity.g.dart
@@ -1,7 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: unused_element
-
 part of 'city_weather_entity.dart';
 
 // **************************************************************************
@@ -37,57 +35,57 @@ Map<String, dynamic> _$ConditionToJson(Condition instance) => <String, dynamic>{
     };
 
 Current _$CurrentFromJson(Map<String, dynamic> json) => Current(
-      lastUpdatedEpoch: json['lastUpdatedEpoch'] as int?,
-      lastUpdated: json['lastUpdated'] as String?,
-      tempC: json['tempC'] as int?,
-      tempF: (json['tempF'] as num?)?.toDouble(),
-      isDay: json['isDay'] as int?,
+      lastUpdatedEpoch: json['last_updated_epoch'] as int?,
+      lastUpdated: json['last_updated'] as String?,
+      tempC: json['temp_c'] as int?,
+      tempF: (json['temp_f'] as num?)?.toDouble(),
+      isDay: json['is_day'] as int?,
       condition: json['condition'] == null
           ? null
           : Condition.fromJson(json['condition'] as Map<String, dynamic>),
-      windMph: (json['windMph'] as num?)?.toDouble(),
-      windKph: (json['windKph'] as num?)?.toDouble(),
-      windDegree: json['windDegree'] as int?,
-      windDir: json['windDir'] as String?,
-      pressureMb: json['pressureMb'] as int?,
-      pressureIn: (json['pressureIn'] as num?)?.toDouble(),
-      precipMm: json['precipMm'] as int?,
-      precipIn: json['precipIn'] as int?,
+      windMph: (json['wind_mph'] as num?)?.toDouble(),
+      windKph: (json['wind_kph'] as num?)?.toDouble(),
+      windDegree: json['wind_degree'] as int?,
+      windDir: json['wind_dir'] as String?,
+      pressureMb: json['pressure_mb'] as int?,
+      pressureIn: (json['pressure_in'] as num?)?.toDouble(),
+      precipMm: json['precip_mm'] as int?,
+      precipIn: json['precip_in'] as int?,
       humidity: json['humidity'] as int?,
       cloud: json['cloud'] as int?,
-      feelslikeC: json['feelslikeC'] as int?,
-      feelslikeF: (json['feelslikeF'] as num?)?.toDouble(),
-      visKm: json['visKm'] as int?,
-      visMiles: json['visMiles'] as int?,
+      feelslikeC: json['feelslike_c'] as int?,
+      feelslikeF: (json['feelslike_f'] as num?)?.toDouble(),
+      visKm: json['vis_km'] as int?,
+      visMiles: json['vis_miles'] as int?,
       uv: json['uv'] as int?,
-      gustMph: (json['gustMph'] as num?)?.toDouble(),
-      gustKph: (json['gustKph'] as num?)?.toDouble(),
+      gustMph: (json['gust_mph'] as num?)?.toDouble(),
+      gustKph: (json['gust_kph'] as num?)?.toDouble(),
     );
 
 Map<String, dynamic> _$CurrentToJson(Current instance) => <String, dynamic>{
-      'lastUpdatedEpoch': instance.lastUpdatedEpoch,
-      'lastUpdated': instance.lastUpdated,
-      'tempC': instance.tempC,
-      'tempF': instance.tempF,
-      'isDay': instance.isDay,
+      'last_updated_epoch': instance.lastUpdatedEpoch,
+      'last_updated': instance.lastUpdated,
+      'temp_c': instance.tempC,
+      'temp_f': instance.tempF,
+      'is_day': instance.isDay,
       'condition': instance.condition,
-      'windMph': instance.windMph,
-      'windKph': instance.windKph,
-      'windDegree': instance.windDegree,
-      'windDir': instance.windDir,
-      'pressureMb': instance.pressureMb,
-      'pressureIn': instance.pressureIn,
-      'precipMm': instance.precipMm,
-      'precipIn': instance.precipIn,
+      'wind_mph': instance.windMph,
+      'wind_kph': instance.windKph,
+      'wind_degree': instance.windDegree,
+      'wind_dir': instance.windDir,
+      'pressure_mb': instance.pressureMb,
+      'pressure_in': instance.pressureIn,
+      'precip_mm': instance.precipMm,
+      'precip_in': instance.precipIn,
       'humidity': instance.humidity,
       'cloud': instance.cloud,
-      'feelslikeC': instance.feelslikeC,
-      'feelslikeF': instance.feelslikeF,
-      'visKm': instance.visKm,
-      'visMiles': instance.visMiles,
+      'feelslike_c': instance.feelslikeC,
+      'feelslike_f': instance.feelslikeF,
+      'vis_km': instance.visKm,
+      'vis_miles': instance.visMiles,
       'uv': instance.uv,
-      'gustMph': instance.gustMph,
-      'gustKph': instance.gustKph,
+      'gust_mph': instance.gustMph,
+      'gust_kph': instance.gustKph,
     };
 
 Location _$LocationFromJson(Map<String, dynamic> json) => Location(
@@ -96,8 +94,8 @@ Location _$LocationFromJson(Map<String, dynamic> json) => Location(
       country: json['country'] as String?,
       lat: (json['lat'] as num?)?.toDouble(),
       lon: (json['lon'] as num?)?.toDouble(),
-      tzId: json['tzId'] as String?,
-      localtimeEpoch: json['localtimeEpoch'] as int?,
+      tzId: json['tz_id'] as String?,
+      localtimeEpoch: json['localtime_epoch'] as int?,
       localtime: json['localtime'] as String?,
     );
 
@@ -107,7 +105,7 @@ Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
       'country': instance.country,
       'lat': instance.lat,
       'lon': instance.lon,
-      'tzId': instance.tzId,
-      'localtimeEpoch': instance.localtimeEpoch,
+      'tz_id': instance.tzId,
+      'localtime_epoch': instance.localtimeEpoch,
       'localtime': instance.localtime,
     };
